f4330b28bed0fa5297d5a4eba4e37fc347037635
JoeFrumenti <joefrumenti@gmail.com>
2025-01-01 14:19:33 -0800

    made sand get dark when mouse goes over it

--- Content/src/entities/Sand.cs
+++ Content/src/entities/Sand.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,19 +12,23 @@ namespace ZenGarden.Content.src.entities
     internal class Sand : UD
     {
         private Rectangle grain;
+        private Color col;
         public Sand(int x, int y, int size) {
             grain = new Rectangle(x,y,size,size);
+            col = new Color(255,255,0);
         }
 
         internal override void Update()
         {
-            
+            MouseState ms = Mouse.GetState();
+            if(grain.Contains(ms.Position))
+                col = new Color(255,200,0);
         }
         internal override void Draw()
         {
             Texture2D tex = new Texture2D(Game1.Instance.spriteBatch.GraphicsDevice, 1, 1);
             tex.SetData(new[] { Color.White });
-            Game1.Instance.spriteBatch.Draw(tex, grain, Color.White);
+            Game1.Instance.spriteBatch.Draw(tex, grain, col);
         }
 
         
