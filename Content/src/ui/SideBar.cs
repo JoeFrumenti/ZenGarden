5e05a7171977ecd4c1d2aa2539dcb84b42fe7e52
JoeFrumenti <joefrumenti@gmail.com>
2025-01-02 17:16:04 -0800

    sidebar can open and close now

--- Content/src/ui/SideBar.cs
+++ Content/src/ui/SideBar.cs
@@ -15,8 +15,9 @@ namespace ZenGarden.Content.src.ui
         private bool open = false;
 
         Texture2D openTex;
+        Texture2D closeTex;
 
-
+        Rectangle closeButton;
         Rectangle openButton;
         Rectangle house;
 
@@ -24,16 +25,19 @@ namespace ZenGarden.Content.src.ui
         {
             height = Game1.Instance.GraphicsDevice.Viewport.Height;
             openTex = Game1.Instance.graphicsHandler.generateTexture("ui\\openTab.png");
+            closeTex = Game1.Instance.graphicsHandler.generateTexture("ui\\closeTab.png");
              
+            closeButton = new Rectangle(width-50,height/2,50,50);
             openButton = new Rectangle(0, height / 2, 50, 50);
             house = new Rectangle((int)anchor.X, (int)anchor.Y, width, height);
         }
         internal override void Update()
         {
             MouseState ms = Game1.Instance.mouseState;
-            if(ms.LeftButton == ButtonState.Pressed) {
+            if(ms.LeftButton == ButtonState.Pressed && Game1.Instance.previousMouseState.LeftButton != ButtonState.Pressed) {
                 if(open) {
-
+                    if(closeButton.Contains(ms.Position))
+                        open = false;
                 }
                 else
                 {
@@ -51,6 +55,8 @@ namespace ZenGarden.Content.src.ui
                 Texture2D tex = Game1.Instance.graphicsHandler.generateTexture(new Color(10, 10, 10));
                 
                 Game1.Instance.spriteBatch.Draw(tex, house, Color.White);
+                Game1.Instance.spriteBatch.Draw(closeTex, closeButton, Color.White);
+
             }
             else 
             { 
